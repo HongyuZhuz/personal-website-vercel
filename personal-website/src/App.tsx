29475b1294c0fb33995692da37bbfb32e4696bac
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-03 17:40:56 +1100

    setted speed dashboard

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -4,11 +4,14 @@ import { AboutMe } from './components/about'
 import { MyEducationSection } from './components/education'
 import { Designer } from './components/designer'
 import { Footer } from './components/footer'
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 function App() {
   
   return (
     <div>
+      <SpeedInsights />
       <HeroSection />
       <AboutMe />
       <MyEducationSection />
