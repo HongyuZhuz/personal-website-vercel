320d4044b2962152690e021fec657faf71fa512a
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-06 21:50:26 +1100

    improved my photo gallary

--- personal-website/src/components/designer.tsx
+++ personal-website/src/components/designer.tsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
-const imageList:string[] = ["https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio-1+.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-2.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-3.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-4.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-5.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-6.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-7.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-8.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-9.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-10.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-11.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-12.jpg",
-"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-13.jpg",]
+
+
+const images = [{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio-1+.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-2.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-3.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-4.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-5.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-6.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-7.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-8.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-9.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-10.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-11.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-12.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/portfolio/Portfolio+-13.jpg"},]
 
 export const Designer=()=>{
     return(
@@ -20,8 +23,8 @@ export const Designer=()=>{
           <div className='flex flex-col  max-w-5xl mx-auto'>
             <Header />
           </div>
-          <div className='flex justify-center'>
-            <ImageBrowser images={imageList}/>
+          <div className=" pb-10 md:pb-20 mt-5 md:px-10 ">
+          <ImageGallery items={images} showThumbnails={false}/>
           </div>
         </div>
         )
@@ -30,7 +33,7 @@ export const Designer=()=>{
 const Header = () =>(
   <>
     <header className="text-white mt-8 ml-8">
-        <div className=" flex flex-col mt-20">
+        <div className=" flex flex-col md:mt-10">
             <h1 className="text-red-500 text-xl font-bold">MY PORTFOLIO</h1>
             <h2 className="text-white text-4xl">As a <span className="font-bold">Designer</span></h2>
             <p className="text-white text-md max-w-xl">At this part, I will introduce what I have done as a designer. My portfolio will tell you the story.</p>
@@ -39,76 +42,4 @@ const Header = () =>(
   </>
     
 )
-
-
-interface ImageBrowserProps {
-  images: string[]; // Array of image URLs
-}
-
-const ImageBrowser: React.FC<ImageBrowserProps> = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex > 0 ? prevIndex - 1 : images.length - 1
-    );
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex < images.length - 1 ? prevIndex + 1 : 0
-    );
-  };
-
-  return (
-    <div className="flex max-w-full mx-auto px-4 md:mb-20 mb-5 overflow-x-auto">
-      <button
-        className="p-4 m-2  rounded shadow text-red-500 font-bold md:text-4xl hover:bg-white text-xl"
-        onClick={goToPrevious}
-      >
-        {'<'}
-      </button>
-      <img
-        src={images[currentIndex]}
-        alt="Displayed content"
-        className="max-w-full max-h-96 mt-4"
-        onClick={handleImageClick}
-      />
-      <button
-        className="p-4 m-2  rounded shadow text-red-500 font-bold md:text-4xl hover:bg-white text-xl"
-        onClick={goToNext}
-      >
-        {'>'}
-      </button>
-    </div>
-  );
-};
-
-const handleImageClick = (event: React.MouseEvent<HTMLImageElement>) => {
-    const elem = event.currentTarget;
-  
-    if (!document.fullscreenElement) {
-      if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-      } else if ((elem as any).mozRequestFullScreen) { /* Firefox */
-        (elem as any).mozRequestFullScreen();
-      } else if ((elem as any).webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-        (elem as any).webkitRequestFullscreen();
-      } else if ((elem as any).msRequestFullscreen) { /* IE/Edge */
-        (elem as any).msRequestFullscreen();
-      }
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if ((document as any).mozCancelFullScreen) { /* Firefox */
-        (document as any).mozCancelFullScreen();
-      } else if ((document as any).webkitExitFullscreen) { /* Chrome, Safari and Opera */
-        (document as any).webkitExitFullscreen();
-      } else if ((document as any).msExitFullscreen) { /* IE/Edge */
-        (document as any).msExitFullscreen();
-      }
-    }
-  };
-
-        
         
--- personal-website/src/components/hobbie.tsx
+++ personal-website/src/components/hobbie.tsx
@@ -1,95 +1,50 @@
-import React, { useState } from 'react';
 
-// Define the type for an array of image URLs
-type ImageArray = string[];
 
-// Define the props for the ImageSlider component
-interface ImageSliderProps {
-  images: ImageArray;
-}
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
-// ImageSlider component
-const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
-  const [current, setCurrent] = useState<number>(0);
 
-  const nextSlide = () => {
-    setCurrent((prevCurrent) => (prevCurrent === images.length - 1 ? 0 : prevCurrent + 1));
-  };
-
-  const prevSlide = () => {
-    setCurrent((prevCurrent) => (prevCurrent === 0 ? images.length - 1 : prevCurrent - 1));
-  };
-
-  if (!Array.isArray(images) || images.length <= 0) {
-    return null;
+interface ImageObject {
+    original: string;
   }
+  
+  type ImageArray = ImageObject[];
 
-  return (
-    <div className="flex justify-center items-center">
-      <button
-        onClick={prevSlide}
-        className="z-10 p-2 text-white bg-black rounded-full shadow-lg mr-2"
-        aria-label="Previous slide"
-      >
-        &lt;
-      </button>
-      {images.map((image, index) => (
-        <div
-          className={`absolute transition-opacity duration-700 ease-in-out ${index === current ? 'opacity-100' : 'opacity-0'}`}
-          key={index}
-        >
-          {index === current && (
-            <img src={image} alt={`Content number ${index}`} className="w-full h-auto" />
-          )}
-        </div>
-      ))}
-      <button
-        onClick={nextSlide}
-        className="z-10 p-2 text-white bg-black rounded-full shadow-lg ml-2"
-        aria-label="Next slide"
-      >
-        &gt;
-      </button>
-    </div>
-  );
-};
 
-// MyHobbyPage component
 export const MyHobbyPage: React.FC = () => {
   const images: ImageArray = [
-    "https://hyzpublic.s3.amazonaws.com/Photo/1.jpg",
-    "https://hyzpublic.s3.amazonaws.com/Photo/2.jpg",
-    "https://hyzpublic.s3.amazonaws.com/Photo/3.jpg",
-    "https://hyzpublic.s3.amazonaws.com/Photo/4.jpg",
-    "https://hyzpublic.s3.amazonaws.com/Photo/5.jpg",
-    "https://hyzpublic.s3.amazonaws.com/Photo/6.jpg",
-
+    {original:"https://hyzpublic.s3.amazonaws.com/Photo/1.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/2.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/3.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/4.jpg",},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/5.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/6.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/7.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/8.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/9.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/10.jpg"},
+{original:"https://hyzpublic.s3.amazonaws.com/Photo/11.jpg"},
   ];
 
   return (
     <div className='min-h-screen bg-black text-white'>
-        <div className="flex flex-col  max-w-5xl mx-auto min-h-screen">
-            <Header/>
-        </div>
-        <div className="relative max-w-md mx-auto">
-
-        </div>
+      <div className="flex flex-col max-w-5xl mx-auto">
+        <Header/>
+        <ImageGallery items={images} showThumbnails={false}/>
+      </div>
     </div>
   );
 };
 
-const Header = () =>(
-    <>
-    <header className="bg-black text-white mt-8 ml-8">
-        <div className=" flex flex-col mt-20">
-            <h1 className="text-red-500 text-xl font-bold">MY HOBBIES</h1>
-            <h2 className="text-white text-4xl">As a <span className="font-bold">Photographer</span></h2>
-            <p className="text-white text-md max-w-xl">During my holiday, I love to travel around and use my camera to record my lovely life. Come and enjoy part of my collection!</p>
-        </div>
-    </header>
-    
-    </>
-    
-)
+const Header = () => (
+  <header className="mt-4 ml-8 mb-10">
+    <div className="flex flex-col mt-20">
+      <h1 className="text-red-500 text-xl font-bold">MY HOBBIES</h1>
+      <h2 className="text-4xl">As a <span className="font-bold">Photographer</span></h2>
+      <p className="text-md max-w-xl">During my holiday, I love to travel around and use my camera to record my lovely life. Come and enjoy part of my collection!</p>
+    </div>
+  </header>
+);
+
 
 
